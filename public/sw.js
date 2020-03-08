4df6c39847e0dd6c0bd504dd9d83a6818a1cf9c9
mikhailevseev <mevseev@zendesk.com>
2020-03-08 15:49:16 +0100

    Added proper caching into the application

---
+++ public/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('v1').then(function (cache) {
+            return cache.addAll([
+                './static/js/bundle.js',
+                './static/js/0.chunk.js',
+                './static/js/main.chunk.js',
+                './manifest.json',
+                './favicon.ico',
+                './logo192.png',
+            ]).then(() => console.log('App cached'));
+        })
+    )
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((resp) => {
+            console.log((!!resp ? 'From cache: ' : 'Fetching: ') + event.request.url);
+            return resp || fetch(event.request);
+        })
+    );
+});
