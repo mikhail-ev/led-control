5213e8071b133d7254794ec06b27135fa575f22a
mikhailevseev <mevseev@zendesk.com>
2020-03-14 19:54:06 +0100

    Fixed SW

--- public/sw-registrator.js
+++ public/sw-registrator.js
@@ -1,3 +1,3 @@
-navigator.serviceWorker.register('js/sw.js').then(
+navigator.serviceWorker.register('sw.js').then(
     () => console.log('Service worker has been installed!'),
     (error) => console.warn('Service worker has not been installed!', error));
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.static('public'))
+let requestCounter = 0;
 
-app.get('/test', (req, res) => res.send('Hello World!'))
+app.use((...args) => {
+    console.log(`[${requestCounter++}][${new Date().toLocaleTimeString()}]${args[0].url}`);
+    express.static('public')(...args);
+})
+
+app.get('*', (req, res) => res.redirect('/'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
