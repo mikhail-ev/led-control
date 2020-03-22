377af828cc633074a09d8cfb3839377ef5894d5c
Mikhail <targettaiga@gmail.com>
2020-03-22 10:26:00 +0100

    Updated sketch

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -2,6 +2,7 @@
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
 
 #ifndef STASSID
 #define STASSID "WLAN1-797A66"
@@ -22,6 +23,8 @@ const char* app = APP_NAME;
 int red = 255;
 int green = 255;
 int blue = 255;
+
+String markup = "";
 
 ESP8266WebServer server(80);
 
@@ -61,6 +64,12 @@ void handleNotFound() {
   digitalWrite(BUILDIN_LED, HIGH);
 }
 
+void handleRoot() {
+  digitalWrite(BUILDIN_LED, LOW);
+  server.send(200, "text/html", markup);
+  digitalWrite(BUILDIN_LED, HIGH);
+}
+
 int covertResolution(int value) {
   return (int) round(constrain(value, 0, 255) / 255.0 * 1023);
 }
@@ -74,16 +83,21 @@ void setColors(int redInput, int greenInput, int blueInput) {
   int blueResolved = covertResolution(blue);
   analogWrite(R_LED, redResolved);
   Serial.print("Setting red: ");
-  Serial.println(redResolved);
+  Serial.print(redResolved);
   analogWrite(G_LED, greenResolved);
-  Serial.print("Setting green: ");
-  Serial.println(greenResolved);
+  Serial.print(" , setting green: ");
+  Serial.print(greenResolved);
   analogWrite(B_LED, blueResolved);
-  Serial.print("Setting blue: ");
+  Serial.print(" , setting blue: ");
   Serial.println(blueResolved);
 }
 
 void blinkTwice(int redValue, int greenValue, int blueValue) {
+  blinkOnce(redValue, greenValue, blueValue);
+  blinkOnce(redValue, greenValue, blueValue);
+}
+
+void blinkOnce(int redValue, int greenValue, int blueValue) {
   int previousRed = red;
   int previousGreen = green;
   int previousBlue = blue;
@@ -91,9 +105,65 @@ void blinkTwice(int redValue, int greenValue, int blueValue) {
   delay(300);
   setColors(previousRed, previousGreen, previousBlue);
   delay(300);
-  setColors(redValue, greenValue, blueValue);
-  delay(300);
-  setColors(previousRed, previousGreen, previousBlue);
+}
+
+boolean loadMarkup() {
+  WiFiClient client;
+  HTTPClient http;
+  boolean success = false;
+
+  char* url = "http://led-control-app.herokuapp.com/markup?app=";
+  strcat(url, app); /* add the extension */
+
+  Serial.print("[HTTP] begin...\n");
+  if (http.begin(client, url)) {
+    Serial.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        markup = http.getString();
+        success = true;
+      }
+    } else {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      if (httpCode == HTTP_CODE_INTERNAL_SERVER_ERROR) {
+        blinkTwice(255, 0, 0);
+      } else {
+        blinkOnce(255, 0, 0);
+      }
+    }
+
+    http.end();
+  } else {
+    Serial.printf("[HTTP} Unable to connect\n");
+    blinkOnce(255, 0, 0);
+  }
+
+  return success;
+}
+
+void connectToWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void setup(void) {
@@ -106,32 +176,26 @@ void setup(void) {
 
   Serial.begin(115200);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
   setColors(255, 255, 255);
 
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
+  connectToWiFi();
+
+  blinkTwice(255, 255, 0);
+
+  if (loadMarkup()) {
+    blinkTwice(0, 0, 255);
+  } else {
+    return;
   }
-
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
-  blinkTwice(0, 255, 0);
 
   delay(1000);
 
   if (MDNS.begin(app)) {
     Serial.println("MDNS responder started");
-    blinkTwice(0, 0, 255);
+    blinkTwice(0, 255, 0);
   }
 
+  server.on("/", handleRoot);
   server.on("/update", handleUpdate);
   server.on("/status", handleStatus);
   server.onNotFound(handleNotFound);
